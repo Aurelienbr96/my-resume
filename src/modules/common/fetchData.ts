5e3825610d078cd005380278f9fbbe21b5418d82
Aurelienbr96 <aurelien.brachet@prium-solutions.com>
2024-03-02 12:57:04 +0100

    feature (5631) break TS for ci testing

--- src/modules/common/fetchData.ts
+++ src/modules/common/fetchData.ts
@@ -2,7 +2,7 @@ import data from "../../assets/profil.json";
 
 export type JSONData = typeof data;
 
-export const fetchData = async (): Promise<JSONData> => {
+export const fetchData = async (): Promise<string> => {
   const response = await fetch("/src/assets/profil.json");
   if (!response.ok) {
     throw new Error("Failed to fetch data");
