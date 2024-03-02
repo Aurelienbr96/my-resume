b4a7d6a0fff56815f88a8bb979365dd029982275
Aurelienbr96 <aurelien.brachet@prium-solutions.com>
2024-03-02 12:58:21 +0100

    feature (5631) fix TS

--- src/modules/common/fetchData.ts
+++ src/modules/common/fetchData.ts
@@ -2,7 +2,7 @@ import data from "../../assets/profil.json";
 
 export type JSONData = typeof data;
 
-export const fetchData = async (): Promise<string> => {
+export const fetchData = async (): Promise<JSONData> => {
   const response = await fetch("/src/assets/profil.json");
   if (!response.ok) {
     throw new Error("Failed to fetch data");
